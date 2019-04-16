d7883dac37e781527195ef6ea0878e626127e922
HeXiaoliang <hel13140302@126.com>
2019-04-16 19:20:01 +0800

    login view

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { Button, Input, Form, FormItem } from 'element-ui';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(Vuex);
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Form);
+Vue.use(FormItem);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/Login/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="login">
+    <transition name="form-fade" mode="in-out">
+      <section class="form_contianer" v-show="showLogin">
+        <div class="manage_tip">
+          <p>elm后台管理系统</p>
+        </div>
+        <el-form :model="loginForm" :rules="rules" ref="loginForm">
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm()" class="submit_btn">登陆</el-button>
+          </el-form-item>
+        </el-form>
+      </section>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      showLogin: true,
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+        ],
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      this.showLogin = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .form-fade-enter-active, .form-fade-leave-active {
+    transition: all 1s;
+  }
+  .form-fade-enter, .form-fade-leave-active {
+    transform: translate3d(0, -50px, 0);
+    opacity: 0;
+  }
+</style>
